b0b2827a495cea5c9b82a777a6dbdf4d670b95d9
Anthony Green <Anthonydgreen90@gmail.com>
2018-06-13 17:25:01 -0700

    Add project files.

---
+++ Lab05-Zoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab05_Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
